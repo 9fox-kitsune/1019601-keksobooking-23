11e274226a1cf0a3bbbfe4a923fff076498c6874
Admin1 <Admin1@Kaena.local>
2021-06-11 13:46:42 +1000

    Написала функции для создания массива из объектов
    Не работает только поле адреса и поле features у объекта offer - пустые значения

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+/* eslint-disable id-length */
+/* eslint-disable prefer-template */
 /* Использованые ссылки:
 https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number/toFixed
 https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
@@ -21,18 +23,13 @@ function getRandomFloatNumber(minFloatNumber, maxFloatNumber, pointRound) {
   return 'Заданные числа не соответствуют условиям!';
 }
 
-getRandomNumber(10, 15);
-getRandomFloatNumber(1.1, 1.5, 3);
+const PRICE_MIN = 5000;
+const PRICE_MAX = 25000;
+const ROOM_MAX = 5;
+const GUESTS_MAX = 10;
+const OBJECT_COUNT = 10;
 
-// В файле main.js на основе написанных в прошлом задании вспомогательных функций напишите необходимые функции для создания массива из 10 сгенерированных JS-объектов. Каждый объект массива — описание похожего объявления неподалёку.
-
-// Структура каждого объекта должна быть следующей:
-
-// author, объект — описывает автора. Содержит одно поле:
-
-// avatar, строка — адрес изображения вида img/avatars/user{{xx}}.png, где {{xx}} — это число от 1 до 10. Перед однозначными числами ставится 0. Например, 01, 02...10. Адреса изображений не повторяются.
-
-const AVATAR = [
+const AVATARS = [
   'img/avatars/user01.png',
   'img/avatars/user02.png',
   'img/avatars/user03.png',
@@ -45,3 +42,91 @@ const AVATAR = [
   'img/avatars/user10.png',
 ];
 
+const titles = 'Сдается отличный вариант!';
+const descriptions = 'Просторная, светлая, уютная';
+
+const TYPES = [
+  'palace',
+  'flat',
+  'house',
+  'bungalow',
+  'hotel',
+];
+
+const CHECKIN_ALL = [
+  '12:00',
+  '13:00',
+  '14:00',
+];
+
+const CHECKOUT_ALL = [
+  '12:00',
+  '13:00',
+  '14:00',
+];
+
+const FEATURES_ITEMS_LIST = [
+  'wifi',
+  'dishwasher',
+  'parking',
+  'washer',
+  'elevator',
+  'conditioner',
+];
+
+const PHOTOS_ITEMS = [
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg',
+];
+
+const getRandomArrayElement = (elements) => elements[getRandomNumber(0, elements.length - 1)];
+
+const authorAvatar = () => ({
+  avatar: getRandomArrayElement(AVATARS),
+});
+
+// features, массив строк — массив случайной длины из значений: wifi, dishwasher, parking, washer, elevator, conditioner. Значения не должны повторяться.
+const featuresLength = getRandomNumber(1, 6);
+const createFeaturesList = () => {
+  const featuresItems = new Array(featuresLength).fill('');
+  for (let i = 0; i <= featuresLength - 1; i++) {
+    const randomIndex = getRandomNumber(0, 5);
+    if (!featuresItems.includes(FEATURES_ITEMS_LIST[randomIndex])) {
+      featuresItems[i] = FEATURES_ITEMS_LIST[randomIndex];
+    } else {
+      i--;
+    }
+  }
+  return featuresItems;
+};
+
+const locations = () => ({
+  lat: getRandomFloatNumber(35.65000, 35.70000, 5),
+  lng: getRandomFloatNumber(139.70000, 139.80000, 5),
+});
+
+const offers = () => ({
+  title: titles,
+  address: locations.lat + ' ' + locations.lng,
+  price: getRandomNumber(PRICE_MIN, PRICE_MAX),
+  type: getRandomArrayElement(TYPES),
+  rooms: getRandomNumber(1, ROOM_MAX),
+  guests: getRandomNumber(1, GUESTS_MAX),
+  checkin: getRandomArrayElement(CHECKIN_ALL),
+  checkout: getRandomArrayElement(CHECKOUT_ALL),
+  features: createFeaturesList,
+  description: descriptions,
+  photos: getRandomArrayElement(PHOTOS_ITEMS),
+});
+
+const objectInfo = () => ({
+  author: authorAvatar(),
+  offer: offers(),
+  location: locations(),
+});
+
+const objectsNearby = new Array(OBJECT_COUNT).fill(null).map(() => objectInfo());
+
+// eslint-disable-next-line no-console
+console.log(objectsNearby);
